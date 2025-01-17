28fd99ea6e5bb49ed241c290d664c54786a359a4
jeel11p <jeel11patel@gmail.com>
2025-01-17 15:10:44 -0800

    RollABall codes

---
+++ Assets/BallController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Rigidbody sphereRigidbody;
+    public float ballSpeed = 200f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Calling the Start method");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 inputVector = Vector2.zero; // intialize our input vector
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector += Vector2.up; // "a += b" <=> "a = a + b"
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector += Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector += Vector2.left;
+        }
+
+        Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
+        sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
+        Debug.Log("Resultant Vector: " + inputVector);
+        Debug.Log("Resultant 3D Vector: " + inputVector);
+    }
+}
